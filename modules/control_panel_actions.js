47124faabb8101a48a52f6ae73b2d5db88741dcd
Matlosh <matlew112@gmail.com>
2023-09-14 21:02:17 +0200

    Fixes

--- modules/control_panel_actions.js
+++ modules/control_panel_actions.js
@@ -1,39 +1,51 @@
-import { SETTINGS_ITEM_NAME, get_random_color_hex } from "reusable";
+import { SETTINGS_ITEM_NAME, get_random_color_hex, display_info_box } from "reusable";
 import template_wheels from "data/wheels.json" assert { type: 'json' };
 import { get_input_template } from "control_panel";
 import { show_wheel } from "mechanics";
 
-const init_wheels_events = (wheels_data) => {
-    const wheel_options = document.querySelectorAll('.choose-wheel');
+const init_wheels_events = (element, wheels_data, wheel_event) => {
+    const wheel_options = document.querySelectorAll('.wheel-entry');
     wheel_options.forEach(option => {
         option.addEventListener('click', e => {
             const wheel_id = option.getAttribute('wheel-id');
             const wheel_data = wheels_data.find((element) => element.id?.toString() === wheel_id);
 
-            wheel_data.elements.forEach(element => {
-                if(!element.hasOwnProperty('color')) element.color = get_random_color_hex();
-                if(!element.hasOwnProperty('text_color')) element.text_color = '#000000'; 
-            });
-        
-            show_wheel(300, 300, 250, wheel_data.elements, true);
+            wheel_event(element, wheel_data);
         });
     });
 };
 
-const change_wheel_event = () => {
+const change_wheel_event = (element, wheel_data) => {
     // Gives random colors to elements if they are not set
-    // wheel_data.elements.forEach(element => {
-    //     if(!element.hasOwnProperty('color')) element.color = get_random_color_hex();
-    //     if(!element.hasOwnProperty('text_color')) element.text_color = '#000000'; 
-    // });
+    wheel_data.elements.forEach(element => {
+        if(!element.hasOwnProperty('color')) element.color = get_random_color_hex();
+        if(!element.hasOwnProperty('text_color')) element.text_color = '#000000'; 
+    });
 
-    // show_wheel(300, 300, 250, wheel_data.elements, true);
-    // continue here with adding events
+    show_wheel(300, 300, 250, wheel_data.elements, true);
+};
+
+const delete_wheel_event = (element, wheel_data) => {
+    let settings = JSON.parse(localStorage.getItem(SETTINGS_ITEM_NAME));
+    if(settings === null) {
+        display_info_box("It's not possible to delete a template wheel.", 'error');
+        return;
+    }
+    let wheels = settings.wheels;
+    
+    const wheel_to_delete_index = wheels.map(entry => entry.id).indexOf(wheel_data.id);
+    wheels.splice(wheel_to_delete_index, 1);
+    settings.wheels = wheels;
+
+    localStorage.setItem(SETTINGS_ITEM_NAME, JSON.stringify(settings));
+    display_info_box('Wheel was successfully deleted.', 'success');
+
+    // display_wheels_to_delete(element);
 };
 
 const display_wheels = (element, additional_classes) => {
-    let wheels = JSON.parse(localStorage.getItem(SETTINGS_ITEM_NAME)).wheels;
-    if(wheels === null) wheels = template_wheels;
+    let wheels = JSON.parse(localStorage.getItem(SETTINGS_ITEM_NAME))?.wheels;
+    if(wheels === undefined) wheels = template_wheels;
 
     wheels.forEach(wheel => {
         let inner_html = `
@@ -49,12 +61,15 @@ const display_wheels = (element, additional_classes) => {
 
 // Display all available wheels to choose in the given element
 const display_available_wheels = (element) => {
-    const wheels = display_wheels(element, 'choose-wheel');
-    init_wheels_events(wheels);
+    // element.innerHTML = '';
+    const wheels = display_wheels(element, 'wheel-entry choose-wheel');
+    init_wheels_events(element, wheels, change_wheel_event);
 };
 
 const display_wheels_to_delete = (element) => {
-    display_wheels(element, 'wheel-to-delete');
+    // element.innerHTML = '';  
+    const wheels = display_wheels(element, 'wheel-entry wheel-to-delete');
+    init_wheels_events(element, wheels, delete_wheel_event);
 };
 
 export { display_available_wheels, display_wheels_to_delete };
